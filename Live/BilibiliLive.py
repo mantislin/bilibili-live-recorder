18e54c35db3f992f8f08984c7696110c9964d819
mantislin <mntslin@hotmail.com>
2019-09-17 18:29:15 +0800

    update;;try to prevent it from stucking by adding a bunch of try catch scopes

--- Live/BilibiliLive.py
+++ Live/BilibiliLive.py
@@ -12,26 +12,36 @@ class BiliBiliLive(BaseLive):
         data = {}
         room_info_url = 'https://api.live.bilibili.com/room/v1/Room/get_info'
         user_info_url = 'https://api.live.bilibili.com/live_user/v1/UserInfo/get_anchor_in_room'
-        response = self.common_request('GET', room_info_url, {'room_id': self.room_id}).json()
-        if response['msg'] == 'ok':
-            data['roomname'] = response['data']['title']
-            data['site_name'] = self.site_name
-            data['site_domain'] = self.site_domain
-            data['status'] = response['data']['live_status'] == 1
-        self.room_id = str(response['data']['room_id'])  # 解析完整 room_id
-        response = self.common_request('GET', user_info_url, {'roomid': self.room_id}).json()
-        data['hostname'] = response['data']['info']['uname']
+        try:
+            response = self.common_request('GET', room_info_url, {'room_id': self.room_id}).json()
+        except Exception as e:
+            print(e)
+            pass
+        else:
+            if response['msg'] == 'ok':
+                data['roomname'] = response['data']['title']
+                data['site_name'] = self.site_name
+                data['site_domain'] = self.site_domain
+                data['status'] = response['data']['live_status'] == 1
+            self.room_id = str(response['data']['room_id'])  # 解析完整 room_id
+            response = self.common_request('GET', user_info_url, {'roomid': self.room_id}).json()
+            data['hostname'] = response['data']['info']['uname']
         return data
 
     def get_live_urls(self):
         live_urls = []
         url = 'https://api.live.bilibili.com/api/playurl'
-        durls = self.common_request('GET', url, {
-            'cid': self.room_id,
-            'otype': 'json',
-            'quality': 0,
-            'platform': 'web'
-        }).json()
-        for durl in durls['durl']:
-            live_urls.append(durl['url'])
+        try:
+            durls = self.common_request('GET', url, {
+                'cid': self.room_id,
+                'otype': 'json',
+                'quality': 0,
+                'platform': 'web'
+            }).json()
+        except Exception as e:
+            print(e)
+            pass
+        else:
+            for durl in durls['durl']:
+                live_urls.append(durl['url'])
         return live_urls
--- run.py
+++ run.py
@@ -14,33 +14,54 @@ class BiliBiliLiveRecorder(BiliBiliLive):
         super().__init__(room_id)
         self.inform = utils.inform
         self.print = utils.print_log
+        self.room_title = None
+        self.host_name = None
 
     def check(self, interval):
         while True:
-            room_info = self.get_room_info()
-            if room_info['status']:
-                self.inform(room_id=self.room_id,desp=room_info['roomname'])
-                self.print(self.room_id, room_info['roomname'])
-                break
-            else:
-                self.print(self.room_id, '等待开播')
+            try:
+                room_info = self.get_room_info()
+                if room_info['status']:
+                    self.inform(room_id=self.room_id,desp=room_info['roomname'])
+                    self.print(self.room_id, room_info['roomname'])
+                    self.room_title = room_info['roomname']
+                    self.host_name = room_info['hostname']
+                    break
+                else:
+                    self.print(self.room_id, '等待开播')
+            except Exception as e:
+                print(e)
+                pass
             time.sleep(interval)
         return self.get_live_urls()
 
     def record(self, record_url, output_filename):
         self.print(self.room_id, '√ 正在录制...' + self.room_id)
-        resp = requests.get(record_url, stream=True)
-        with open(output_filename, "wb") as f:
-            for chunk in resp.iter_content(chunk_size=512):
-                f.write(chunk) if chunk else None
+        try:
+            resp = requests.get(record_url, stream=True)
+        except Exception as e:
+            print(e)
+            pass
+        else:
+            try:
+                with open(output_filename, "wb") as f:
+                    for chunk in resp.iter_content(chunk_size=1024*3):
+                        f.write(chunk) if chunk else None
+            except Exception as e:
+                print(e)
+                pass
 
     def run(self):
         while True:
-            urls = self.check(interval=5*60)
-            filename = utils.generate_filename(self.room_id)
-            c_filename = os.path.join(os.getcwd(), 'files', filename)
-            self.record(urls[0], c_filename)
-            self.print(self.room_id, '录制完成')
+            try:
+                urls = self.check(interval=10)
+                filename = utils.generate_filename(self.host_name + '__' + self.room_title, self.room_id)
+                c_filename = os.path.join(os.getcwd(), 'files', filename)
+                self.record(urls[0], c_filename)
+                self.print(self.room_id, '录制完成')
+            except Exception as e:
+                print(e)
+                pass
 
 
 if __name__ == '__main__':
